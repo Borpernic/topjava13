8dc687c2ae925b473233277369cbd3657f2eb658
borpernic <borpernic@gmail.com>
2018-08-31 15:35:15 +0300

    7 02 HW6 meals

--- src/main/java/ru/javawebinar/topjava/web/meal/AbstractMealController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/AbstractMealController.java
@@ -1,17 +1,28 @@
 package ru.javawebinar.topjava.web.meal;
 
-import org.springframework.stereotype.Controller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.javawebinar.topjava.AuthorizedUser;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.to.MealWithExceed;
+import ru.javawebinar.topjava.util.DateTimeUtil;
+import ru.javawebinar.topjava.util.MealsUtil;
 
-@Controller
-public class MealRestController {
-    private static final Logger log = LoggerFactory.getLogger(MealRestController.class);
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
 
-    private final MealService service;
+import static ru.javawebinar.topjava.util.Util.orElse;
+import static ru.javawebinar.topjava.util.ValidationUtil.assureIdConsistent;
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
+
+public abstract class AbstractMealController {
+    private final Logger log = LoggerFactory.getLogger(getClass());
 
     @Autowired
-    public MealRestController(MealService service) {
-        this.service = service;
-    }
+    private MealService service;
 
     public Meal get(int id) {
         int userId = AuthorizedUser.id();
@@ -60,6 +71,6 @@ public class MealRestController {
 
         return MealsUtil.getFilteredWithExceeded(mealsDateFiltered, AuthorizedUser.getCaloriesPerDay(),
                 orElse(startTime, LocalTime.MIN), orElse(endTime, LocalTime.MAX)
-                );
+        );
     }
 }
